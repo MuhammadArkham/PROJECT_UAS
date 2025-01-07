a76f3ccd69155fff853c53a4b4cbc3047cb9aedc
MuhammadArkham <arkhamqwe123@gmail.com>
2025-01-08 00:43:52 +0700

    Add files via upload

--- Data/data.py
+++ Data/data.py
@@ -1,7 +1,7 @@
 class Data:
     def __init__(self):
         self.tiket = {
-            'REGULER': {'harga': 150000, 'stok': 100},
+            'REGULER': {'harga': 250000, 'stok': 100},
             'VIP': {'harga': 500000, 'stok': 50},
             'VVIP': {'harga': 1000000, 'stok': 25}
         }
